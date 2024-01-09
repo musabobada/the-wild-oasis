8843db3dcc7fa3aefb97e4f06bce5ce518c53a1a
musabobada <musab012488708@gmail.com>
2024-01-10 01:58:20 +0200

    up

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,4 @@
-import useCabins from "./UseCabins";
+import useCabins from "./useCabins";
 
 import CabinRow from "./CabinRow";
 import Spinner from "../../ui/Spinner";
@@ -9,25 +9,24 @@ import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { cabins, isLoading } = useCabins();
-  const [searchParams] = useSearchParams()
-  const filterValue = searchParams.get("discount") || "all"
+  const [searchParams] = useSearchParams();
+  const filterValue = searchParams.get("discount") || "all";
 
-  let filteredCabins
-  if (filterValue === "all") filteredCabins = cabins
-  if (filterValue === "no-discount") filteredCabins = cabins?.filter(cabin => cabin.discount === 0)
-  if (filterValue === "with-discount") filteredCabins = cabins?.filter(cabin => cabin.discount > 0)
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount") filteredCabins = cabins?.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount") filteredCabins = cabins?.filter((cabin) => cabin.discount > 0);
 
-  const sortBy = searchParams.get("sortBy") || "startDate-asc"
-  const [field, direction] = sortBy.split("-")
-  const modifier = direction === "asc" ? 1 : -1
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
   const sortedCabin = filteredCabins?.sort((a, b) => {
     if (field === "name") {
-      return (a[field].localeCompare(b[field])) * modifier
+      return a[field].localeCompare(b[field]) * modifier;
+    } else {
+      return (a[field] - b[field]) * modifier;
     }
-    else {
-      return (a[field] - b[field]) * modifier
-    }
-  })
+  });
 
   if (isLoading) return <Spinner />;
   if (!cabins.length) return <Empty resource="cabins" />;
@@ -42,10 +41,7 @@ function CabinTable() {
           <div>Discount</div>
           <div></div>
         </Table.Header>
-        <Table.Body
-          data={sortedCabin}
-          render={(cabin, i) => (<CabinRow key={i} cabin={cabin} />)}
-        />
+        <Table.Body data={sortedCabin} render={(cabin, i) => <CabinRow key={i} cabin={cabin} />} />
       </Table>
     </Menus>
   );
