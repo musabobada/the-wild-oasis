2e723ec1d7a74f778558fd0a89ed01451f8d93e4
musabobada <musab012488708@gmail.com>
2024-01-10 02:15:01 +0200

    up

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://zbsgbyvkdxdqwihcabsp.supabase.co";
 export const supabaseStorage = `${supabaseUrl}/storage/v1/object/public`;
-export const supabaseStorageCabinImages = `${supabaseStorage}/cabin-images/ff`;
+export const supabaseStorageCabinImages = `${supabaseStorage}/cabin-images/`;
 export const supabaseStorageAvatars = `${supabaseStorage}/avatars`;
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpic2dieXZrZHhkcXdpaGNhYnNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQwMzEzODAsImV4cCI6MjAxOTYwNzM4MH0.ZO0oGGI7lPsZEMvKCcBPTxny8FP-cjjySLH4-f1WjUA";
