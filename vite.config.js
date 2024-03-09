1f5beda4c96500950b8b3b741e1ca324ef569bfb
musabobada <musab012488708@gmail.com>
2024-03-09 12:32:24 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/the-wild-oasis/",
   plugins: [react(), eslint()],
   server: {
     host: true,
